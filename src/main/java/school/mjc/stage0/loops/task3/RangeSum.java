c06d20579f7aa91aab4e6fd4acc2037f71be9f3b
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 16:29:35 +0300

    Adding: RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int min = firstBoarder;
+        int max = secondBoarder;
+        int sum = 0;
+
+        if (firstBoarder > secondBoarder) {
+            min = secondBoarder;
+            max = firstBoarder;
+        }
+
+        for(int iterator = min; iterator <= max; iterator++){
+            sum +=iterator;
+        }
+        System.out.println(sum);
     }
 }
