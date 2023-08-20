88652dc5ba75507e01fc2ac90c4a30594b39bae8
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 16:59:06 +0300

    Adding: DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int number){
+        int sum = 0;
+        int updatedNumber = number;
 
+        for(int iterator = 0; iterator < (number + "").length(); iterator++) {
+            sum += updatedNumber % 10;
+            updatedNumber /= 10;
+        }
+
+        if (sum < 0) {
+            sum *= -1;
+        }
+        System.out.println(sum);
     }
 }
