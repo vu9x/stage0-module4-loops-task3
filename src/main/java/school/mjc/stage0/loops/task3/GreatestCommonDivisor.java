8f8492e5f5122e68c12309a78bdb7aec94abc9f2
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 16:44:31 +0300

    Adding: GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int max = first;
+        int result = 1;
+
+        if (second > first) {
+            max = second;
+        }
+
+        for(int iterator = 1; iterator <= max; iterator++) {
+            if (first % iterator == 0 && second % iterator == 0)
+                result = iterator;
+        }
+
+        System.out.println(result);
     }
 }
