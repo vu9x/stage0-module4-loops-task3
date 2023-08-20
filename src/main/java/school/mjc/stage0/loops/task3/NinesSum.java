bb37844b9dfc0ff037e4ad75433e75e6e91fd630
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 17:09:38 +0300

    Adding: NineSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int sum = 0;
+        int nineNumber = 0;
 
+        for (int i = 0; i < lengthOfLastNumber; i++) {
+            nineNumber = nineNumber * 10 + 9;
+            sum = sum + nineNumber;
+        }
+        System.out.println(sum);
     }
 }
