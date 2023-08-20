a66b808b3be90f885adee1a9632815b1e1826ff1
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 16:35:00 +0300

    Adding: AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char a = 'a';
+        char z = 'z';
+
+        for (int iterator = a; iterator <= z; iterator++)
+            System.out.println((char)iterator);
     }
 }
