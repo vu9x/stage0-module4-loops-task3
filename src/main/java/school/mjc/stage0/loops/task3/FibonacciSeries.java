52248181e888d0627618aeb8ca6bfe4cfc1eaec2
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 17:31:03 +0300

    Adding: FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int first = 0;
+        int second = 1;
+        int next;
+
+        for(int i = 0; i < lastFibonacci; i++) {
+            System.out.println(first);
+
+            next = first + second;
+            first = second;
+            second = next;
+        }
     }
 }
